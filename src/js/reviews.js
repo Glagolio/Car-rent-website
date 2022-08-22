0fbf0f98aa7f8ccd990d2e1053bfa18e8c22ea1a
Mykhailo Shyhin <johnglagol@gmail.com>
2022-08-22 18:57:15 +0300

    work with js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,3 +1,4 @@
+import { points, moreText } from './vewportSize';
 const loadMoreBtn = document.querySelectorAll('.js-show-all-button');
 const reviews = document.querySelectorAll('.js-rivews__member');
 
@@ -8,6 +9,14 @@ const isOpen = () => {
   loadMoreBtn.forEach(btn => {
     btn.classList.toggle('hidden');
   });
+  if (
+    moreText.find(el => el.classList.contains('hidden') == true) ||
+    (points.find(el => el.classList.contains('hidden') == true) &&
+      window.innerWidth < 768)
+  ) {
+    moreText.forEach(el => el.classList.toggle('hidden'));
+    points.forEach(el => el.classList.toggle('hidden'));
+  }
 };
 
 loadMoreBtn.forEach(btn => {
--- src/js/vewportSize.js
+++ src/js/vewportSize.js
@@ -1,13 +1,27 @@
 const starIcons = document.querySelectorAll('.fa-star');
+export const points = document.querySelectorAll('.js-points');
+export const moreText = document.querySelectorAll('.js-more-text');
 
 window.addEventListener('resize', () => {
   if (window.innerWidth < 768) {
     starIcons.forEach(item => {
       item.classList.add('fa-xs');
     });
+    moreText.forEach(el => {
+      el.classList.add('hidden');
+    });
+    points.forEach(el => {
+      el.classList.remove('hidden');
+    });
   } else {
     starIcons.forEach(item => {
       item.classList.remove('fa-xs');
+    });
+    moreText.forEach(el => {
+      el.classList.remove('hidden');
+    });
+    points.forEach(el => {
+      el.classList.add('hidden');
     });
   }
 });
