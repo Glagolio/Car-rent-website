faeb6bec0fa1a141fb5668e47bc770705343da96
Mykhailo Shyhin <johnglagol@gmail.com>
2022-08-23 13:30:26 +0300

    done recent-car block

---
+++ src/js/swiper.js
@@ -0,0 +1,10 @@
+import Swiper from 'swiper';
+
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
